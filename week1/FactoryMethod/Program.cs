e031c863d7b23fa77cd693188c06a58d1be5a7b9
prajithxls <prajith221204@gmail.com>
2025-06-20 17:29:23 +0530

    Design pattern assesment

---
+++ week1/FactoryMethod/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+namespace FactoryMethodPatternExample {
+    public interface IDocument {
+        void Open();
+    }
+    public class WordDocument : IDocument {
+        public void Open() {
+            Console.WriteLine("Opened Word Document");
+        }
+    }
+    public class PdfDocument : IDocument {
+        public void Open() {
+            Console.WriteLine("Opened PDF Document");
+        }
+    }
+    public class ExcelDocument : IDocument {
+        public void Open() {
+            Console.WriteLine("Opened Excel Document");
+        }
+    }
+    public abstract class DocumentFactory {
+        public abstract IDocument CreateDocument();
+    }
+    public class WordDocumentFactory : DocumentFactory {
+        public override IDocument CreateDocument() {
+            return new WordDocument();
+        }
+    }
+    public class PdfDocumentFactory : DocumentFactory {
+        public override IDocument CreateDocument() {
+            return new PdfDocument();
+        }
+    }
+    public class ExcelDocumentFactory : DocumentFactory {
+        public override IDocument CreateDocument() {
+            return new ExcelDocument();
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            DocumentFactory wordFactory=new WordDocumentFactory();
+            IDocument word=wordFactory.CreateDocument();
+            word.Open();
+
+            DocumentFactory pdfFactory=new PdfDocumentFactory();
+            IDocument pdf=pdfFactory.CreateDocument();
+            pdf.Open();
+
+            DocumentFactory excelFactory=new ExcelDocumentFactory();
+            IDocument excel=excelFactory.CreateDocument();
+            excel.Open();
+        }
+    }
+}
---
+++ week1/SingletonPattern/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace SingletonPatternExample {
+    public class Logger {
+        private static Logger?_instance;
+        private static readonly object _lock=new();
+        private Logger() {
+            Console.WriteLine("Logger started");
+        }
+        public static Logger GetInstance(){
+            if (_instance == null){
+                lock (_lock){
+                    if (_instance == null){
+                        _instance = new Logger();
+                    }
+                }
+            }
+            return _instance;
+        }
+        public void Log(string msg) {
+            Console.WriteLine("LOG: "+msg);
+        }
+    }
+    class Program {
+        static void Main(string[] args) {
+            var logger1=Logger.GetInstance();
+            logger1.Log("logger1 initialized");
+
+            var logger2=Logger.GetInstance();
+            logger2.Log("logger2 initialized");
+
+            if (logger1==logger2) {
+                Console.WriteLine("Same instance is initialized");
+            } else {
+                Console.WriteLine("Different instances");
+            }
+        }
+    }
+}
