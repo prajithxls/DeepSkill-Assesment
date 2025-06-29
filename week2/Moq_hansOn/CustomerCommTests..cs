206287088c193ec9e9a4a1f1e0697c0f312c2b64
prajithxls <prajith221204@gmail.com>
2025-06-29 10:16:23 +0530

    Nunit and Moq

---
+++ week2/Moq_hansOn/CustomerCommTests..cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        [Test]
+        public void SendMailToCustomer_ShouldReturnTrue()
+        {
+            var mockMailSender = new Mock<IMailSender>();
+            mockMailSender.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            var obj = new CustomerCommunication(mockMailSender.Object);
+
+            var result = obj.SendMailToCustomer();
+
+            Assert.That(result, Is.True);
+        }
+    }
+}
