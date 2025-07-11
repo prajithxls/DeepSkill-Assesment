e9f80c73f3b90178133fcf4eb002aa7a6f08b071
prajithxls <prajith221204@gmail.com>
2025-07-11 19:31:34 +0530

    week4

---
+++ week4/webApi_2/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private static List<string> values = new List<string> { "value1", "value2" };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(values);
+        }
+
+    
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            if (id < 0 || id >= values.Count)
+                return NotFound("Value not found");
+            return Ok(values[id]);
+        }
+
+
+        [HttpPost]
+        public ActionResult Post([FromBody] string value)
+        {
+            values.Add(value);
+            return Ok("Added");
+        }
+
+
+        [HttpPut("{id}")]
+        public ActionResult Put(int id, [FromBody] string value)
+        {
+            if (id < 0 || id >= values.Count)
+                return BadRequest("Invalid ID");
+            values[id] = value;
+            return Ok("Updated");
+        }
+
+
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            if (id < 0 || id >= values.Count)
+                return BadRequest("Invalid ID");
+            values.RemoveAt(id);
+            return Ok("Deleted");
+        }
+    }
+}
